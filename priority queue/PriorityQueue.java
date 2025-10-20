d97b882ff22da8ed3dbea6b47e645edf486399af
Jashan Sehdev <sehdevjashan321@gmail.com>
2025-10-21 01:34:33 +0530

    deleting .exe and .class files

---
+++ priority queue/PriorityQueue.java
@@ -0,0 +1,19 @@
+class PriorityQueue {
+    Node[] arr;
+    int size;
+    
+    PriorityQueue (int capacity) {
+    arr = new Node[capacity];
+    size = 0;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Node <type1> {
+    type1 data;
+    int priority;
+}
